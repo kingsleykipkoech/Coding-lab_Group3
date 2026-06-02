bf22de11138012ef2d784bff34750cc235702e1a
Kingsley Kipkoech <k.kipkoech@alustudent.com>
2026-06-02 23:06:45 +0200

    Wrote the  script  to  calculate  the  avarage water usage for the ICU_WATER_RESERVE

--- hospital_analysis.sh
+++ hospital_analysis.sh
@@ -8,3 +8,11 @@ Print 'Timestamp:"$1" | Device_ID: "$2" | Value:"$4"}'>$REPORT_FILE
 echo"scan complete"
 }
 process_vitals
+
+
+water_audit() {
+echo "checking avarage  water  usage  for  the  ICU_WATER_RESERVE"
+grep " ICU_WATER_RESERVE" active_logs/water_usage_log.log | \
+awk -F'|' '{sum += $3; count++} END {printf "Average ICU Water Usage: %.2f\n", sum/count}'
+}
+water_audit

260f87ec42f9f7243f58fff3464e55c532d3ffb2
Gabriel Agaba <g.agaba@alustudent.com>
2026-06-05 01:44:59 +0200

    modified admin.sh

--- hospital_admin.sh
+++ hospital_admin.sh
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-i#!/bin/bash
-initialize_system() { 
-dir1="active_logs"
-dir2="archived_logs"
-dir3="reports"
-
-if [[ -d "$dir1" ]]; then
-echo "$dir1 directory exists."
-else
-mkdir "$dir1"
-echo "$dir1 has been created. You can check it out."
-fi
-if  [[ -d "$dir2" ]]; then
-echo "$dir2 directory exists."
-else
-mkdir "$dir2"
-echo "$dir2 has been created. You can check it out."
-fi
-=======
 #!/bin/bash
 
 initialize_system() {
@@ -47,12 +27,7 @@ initialize_system() {
 	echo "$dir3 has been created. You can check it out."
 	fi
 }
-secure_data() {
-	echo "Securing the active_logs directory..."
-	chmod 700 active_logs
-	echo "Permissions updated."
->>>>>>> LilianCynthia
-}
+
 secure_data() {
 	echo"we are securing the data"
 	chmod 700 active_logs
@@ -61,9 +36,4 @@ secure_data() {
 } 
 initialize_system
 secure_data
-<<<<<<< HEAD
 echo " System Environment Secured"
-
-=======
-echo "System Environment Secured [$(date)]"
->>>>>>> LilianCynthia

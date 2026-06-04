f52a2b37ae3b70fb6fc80e84085bf6605d00711f
Vanessa Kampiire <v.kampiire@alustudent.com>
2026-06-05 00:41:06 +0200

    Modified the Hospital analysis.sh

--- hospital_analysis.sh
+++ hospital_analysis.sh
@@ -4,6 +4,7 @@ grep "CRITICAL" active_logs/heart_rate_log.log active_logs/temperature_log.log |
 awk -F'|' '{print $1, $2, $3}'>reports/critical_alerts.txt
 echo "scan complete"
 }
+
 water_audit() {
 echo "checking avarage  water  usage  for  the  ICU_WATER_RESERVE"
 grep " ICU_WATER_RESERVE" active_logs/water_usage_log.log | \

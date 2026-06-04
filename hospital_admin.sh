b9ec7141854df18ca78abaecf49baecd12024255
Lilian Kamikazi <l.kamikazi@alustudent.com>
2026-06-05 01:03:06 +0200

    Modified the Hospital admin.sh

--- hospital_admin.sh
+++ hospital_admin.sh
@@ -1,21 +1,31 @@
-i#!/bin/bash
-initialize_system() { 
-dir1="active_logs"
-dir2="archived_logs"
-dir3="reports"
+#!/bin/bash
 
-if [[ -d "$dir1" ]]; then
-echo "$dir1 directory exists."
-else
-mkdir "$dir1"
-echo "$dir1 has been created. You can check it out."
-fi
-if  [[ -d "$dir2" ]]; then
-echo "$dir2 directory exists."
-else
-mkdir "$dir2"
-echo "$dir2 has been created. You can check it out."
-fi
+initialize_system() {
+    dir1="active_logs"
+    dir2="archived_logs"
+    dir3="reports"
+
+	if [[ -d "$dir1" ]]; then
+        echo "$dir1 directory exists."
+	else
+        mkdir "$dir1"
+        echo "$dir1 has been created. You can check it out."
+	fi
+
+	if [[ -d "$dir2" ]]; then
+        echo "$dir2 directory exists."
+	else
+        mkdir "$dir2"
+	echo "$dir2 has been created. You can check it out."
+	fi
+
+
+	if [[ -d "$dir3" ]]; then
+        echo "$dir3 directory exists."
+	else
+	mkdir "$dir3"
+	echo "$dir3 has been created. You can check it out."
+	fi
 }
 secure_data() {
 	echo"we are securing the data"

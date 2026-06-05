f19a2804201a43da222cb9f0297c1590fa5a6124
Vanessa Kampiire <v.kampiire@alustudent.com>
2026-06-05 13:12:01 +0200

    Updated hospital_analysis.sh from main

--- hospital_analysis.sh
+++ hospital_analysis.sh
@@ -1,18 +1,14 @@
 process_vitals() {
 echo "scanning for critical conditions"
-grep "CRITICAL" $activelogs/heart_rate_log.log $activelogs/temprature_log.log |awk -F'|' '{
-$1=Timestamp ,$2=Device_ID 
-$3=Status
-$4=Value
-Print 'Timestamp:"$1" | Device_ID: "$2" | Value:"$4"}'>$REPORT_FILE
-echo"scan complete"
+grep "CRITICAL" active_logs/heart_rate_log.log active_logs/temperature_log.log | \
+awk -F'|' '{print $1, $2, $3}'>reports/critical_alerts.txt
+echo "scan complete"
 }
-process_vitals
-
 
 water_audit() {
 echo "checking avarage  water  usage  for  the  ICU_WATER_RESERVE"
 grep " ICU_WATER_RESERVE" active_logs/water_usage_log.log | \
 awk -F'|' '{sum += $3; count++} END {printf "Average ICU Water Usage: %.2f\n", sum/count}'
 }
+process_vitals
 water_audit
